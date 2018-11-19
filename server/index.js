1f257e1cb5429512239f518f2def75bf44302fa6
Gene Koval <kovale8@students.rowan.edu>
2018-11-19 02:20:39 -0500

    Capitalized sentiment

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+hbs.registerHelper('capitalize', string =>
+    string[0].toUpperCase() + string.slice(1));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,18 +25,18 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
