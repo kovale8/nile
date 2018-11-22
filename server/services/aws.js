54926f2e6d6ed2db6dc3d8050a74bdff6a5e228c
Gene Koval <kovale8@students.rowan.edu>
2018-11-22 06:43:48 -0500

    Added access creds to aws

--- server/services/aws.js
+++ server/services/aws.js
@@ -1,5 +1,7 @@
 const AWS = require('aws-sdk');
 
+AWS.config.accessKeyId = process.env.ACCESS_KEY_ID;
+AWS.config.secretAccessKey = process.env.SECRET_ACCESS_KEY;
 AWS.config.update({region: 'us-east-1'});
 
 module.exports = AWS;
